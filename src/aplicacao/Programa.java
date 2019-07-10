207be6c061a7b82f179c4f26c3fd6149825d5173
Othon-Code <othonn.ssousa@gmail.com>
2019-07-10 09:37:45 -0300

    Vendedor entity class

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,13 +1,16 @@
 package aplicacao;
 
+import java.util.Date;
+
 import model.entidades.Departamento;
+import model.entidades.Vendedor;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		Departamento obj = new Departamento(1, "Livros");
-		
-		System.out.println(obj.toString());
+		Vendedor vend = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(vend);
 		
 		
 		
