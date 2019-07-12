81df33ee251a1fa81e278291dc2c0ed66f5e54a5
Othon-Code <othonn.ssousa@gmail.com>
2019-07-12 14:19:55 -0300

    Implementação do finfById

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.VendDaoJdbc;
 
 public class DaoFactory {
 	
 	public static VendedorDao createVendDao() {
-		return new VendDaoJdbc();
+		return new VendDaoJdbc(DB.getConnection());
 	}
 
 }
--- src/model/dao/impl/VendDaoJdbc.java
+++ src/model/dao/impl/VendDaoJdbc.java
@@ -1,34 +1,81 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.VendedorDao;
+import model.entidades.Departamento;
 import model.entidades.Vendedor;
 
 public class VendDaoJdbc implements VendedorDao {
 
+	private Connection conn;
+
+	public VendDaoJdbc(Connection con) {
+		this.conn = con;
+	}
+
 	@Override
 	public void insert(Vendedor obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Vendedor obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public Vendedor findById(Integer ig) {
-		// TODO Auto-generated method stub
-		return null;
+	public Vendedor findById(Integer id) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName " 
+			+ "FROM seller INNER JOIN department "
+			+ "ON seller.DepartmentId = department.Id " 
+			+ "WHERE seller.Id = ?");
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if (rs.next()) {
+				Departamento dep = new Departamento();
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setNome(rs.getString("DepName"));
+
+				Vendedor vend = new Vendedor();
+				vend.setId(rs.getInt("Id"));
+				vend.setNome(rs.getString("Name"));
+				vend.setEmail(rs.getString("Email"));
+				vend.setSalBase(rs.getDouble("BaseSalary"));
+				vend.setAniv(rs.getDate("BirthDate"));
+				vend.setDepartamento(dep);
+				return vend;
+
+			}
+			return null;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+			
+		}
 	}
 
 	@Override
@@ -36,6 +83,5 @@ public class VendDaoJdbc implements VendedorDao {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
 
 }
